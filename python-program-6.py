b7b6f6a60d4ba29794c4da2638c8ab1ff1015e28
Jason Trikakis <jason_trikakis@mac.com>
2016-12-02 15:39:55 -0800

    Add files via upload

---
+++ python-program-6.py
@@ -0,0 +1,8 @@
+# 6. Write a Python program which accepts a sequence of comma-separated numbers from user
+# and generate a list and a tuple with those numbers.
+
+numbers = input("Enter some comma separated numbers: ")
+list = numbers.split(",")
+tuple = tuple(list)
+print("List: ", list)
+print("Tuple: ", tuple)
