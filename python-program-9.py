70d7255fe4d6954593d9f0a3e9cecf1a15c95c5b
Jason Trikakis <jason_trikakis@mac.com>
2016-12-05 09:45:34 -0800

    Add files via upload

---
+++ python-program-9.py
@@ -0,0 +1,5 @@
+# 9. Write a Python program to display the examination schedule (extract the date from exam_st_date).
+# Note - "%d" and %i are the same thing
+
+exam_st_date = (8, 1, 2016)
+print("The examination will start on %d/%d/%d" % exam_st_date)
